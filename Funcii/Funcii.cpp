fc3d8ef66f5d41a173cc63bbb75af14ccc589aed
234865 <234865@KB37-309-C05>
2024-10-11 10:15:54 +0300

    Добавьте файлы проекта.

---
+++ Funcii/Funcii.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+void proverka(int a) {    
+    if (a < 0) {
+        cout << a << " - число отрицательное" << endl;
+    }
+    else if (a > 0) {
+        cout << a << " - положительное" << endl;
+    }
+}
+
+void srz(int a, int b) { 
+    int srz = (a + b) / 2;
+    cout << "Среднее значение = " << srz << endl;
+}
+
+void chet_nechet(int a) { 
+    if (a % 2 == 0) {
+        cout << "Число чётное" << endl;
+    }
+    else if (a % 2 != 0) {
+        cout << "Число нечётное" << endl;
+    }
+}
+
+int main() {
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int a;
+    cout << "Введите число:" << endl;
+    cin >> a;
+    proverka(a);
+
+    int b, c;
+    cout << "Введите два числа для поиска среднего:" << endl;
+    cin >> b >> c;
+    srz(b, c);
+
+    int d;
+    cout << "Введите число на проверку чётности:" << endl;
+    cin >> d;
+    chet_nechet(d);
+}
